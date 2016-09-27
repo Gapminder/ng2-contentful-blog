9b6d2350d0a66e6922c4c171c2f6fd04326d3985
natali sol <natali.abyss@gmail.com>
2016-09-27 11:55:12 +0300

    fix(markdown): fix empty markdown

--- components/entries-view/html-entry.component.ts
+++ components/entries-view/html-entry.component.ts
@@ -1,6 +1,10 @@
-import { Component, ViewEncapsulation, Input, ViewChild, ElementRef, AfterViewInit, Renderer } from '@angular/core';
+import {
+  Component, ViewEncapsulation, Input, ViewChild, ElementRef, AfterViewInit, Renderer,
+  OnInit
+} from '@angular/core';
 import { AbstractEntry } from './abstract-entry.component';
 import { ContentfulHtmlBlock } from '../contentful/aliases.structures';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'gm-html-entry',
@@ -10,13 +14,20 @@ import { ContentfulHtmlBlock } from '../contentful/aliases.structures';
   `,
   styles: [require('./entries-html.css') as string]
 })
-export class HtmlEntryComponent extends AbstractEntry implements AfterViewInit {
+
+export class HtmlEntryComponent extends AbstractEntry implements AfterViewInit, OnInit {
   @Input() protected entry: ContentfulHtmlBlock;
   @ViewChild('backgroundOwner') private backgroundOwner: ElementRef;
 
   public constructor(renderer: Renderer,
                      elementRef: ElementRef) {
     super(renderer, elementRef);
+  }
+
+  public ngOnInit(): void {
+    if (_.isEmpty(this.entry.fields.content)) {
+      this.entry.fields.content = '';
+    }
   }
 
   public ngAfterViewInit(): void {
