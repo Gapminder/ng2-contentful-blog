bccc67d41c04a793fecfc81efd379f2ecf2de47c
natali sol <natali.abyss@gmail.com>
2016-10-25 14:53:58 +0300

    core(cover): update style

--- demo/constants.js
+++ demo/constants.js
@@ -4,8 +4,8 @@ const HOME_TAG = 'home';
 const PROJECT_TAG = 'gapminder-org';
 
 module.exports = {
-  PROJECT_TAG,
-  HOME_TAG,
+  PROJECT_TAG: PROJECT_TAG,
+  HOME_TAG: HOME_TAG,
   SHARE_FOOTER_LINE_LOGO_TITLE: 'gapminder-mini',
   FOOTER_LOGO_TITLE: 'footer-logo',
   HEADER_LOGO_TITLE: 'gapminder-logo',
